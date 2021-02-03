54d800d98caf327c450ee139846888edf4240b41
jfcumsille <jfcumsille@uc.cl>
2021-02-03 14:38:27 -0300

    add iframe

---
+++ pages/iframe.js
@@ -0,0 +1,12 @@
+const iframe = () => {
+  return (
+    <div>
+      <Head>
+        <title>Iframe</title>
+      </Head>
+      <iframe src="https://85696b10fce2.ngrok.io"></iframe>
+    </div>
+  )
+}
+
+export default iframe

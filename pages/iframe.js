0b6d793212dd8fd84a6d1c2f983cdaaa9de72968
jfcumsille <jfcumsille@uc.cl>
2021-02-04 17:43:01 -0300

    iframe to staging

--- pages/iframe.js
+++ pages/iframe.js
@@ -1,10 +1,12 @@
+import Head from "next/head"
+
 const iframe = () => {
   return (
     <div>
       <Head>
         <title>Iframe</title>
       </Head>
-      <iframe src="https://85696b10fce2.ngrok.io"></iframe>
+      <iframe src="https://staging.keyclouding.cl/applicant?continuum_token=untoken2" height="1024" width="1800"></iframe>
     </div>
   )
 }

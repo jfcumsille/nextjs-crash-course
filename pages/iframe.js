7ffa0b73452debf5b88d50e3b284b4a953d881b1
jfcumsille <jfcumsille@uc.cl>
2021-02-11 11:21:42 -0300

    new url

--- pages/iframe.js
+++ pages/iframe.js
@@ -6,7 +6,12 @@ const iframe = () => {
       <Head>
         <title>Iframe</title>
       </Head>
-      <iframe src="https://staging.keyclouding.cl/applicant?continuum_token=untoken2" height="1024" width="1800"></iframe>
+      <embed id="testContinuum"
+          title="Test Continuum"
+          width="1024"
+          height="720"
+          src="https://2a26269777f5.ngrok.io/applicant?continuum_token=token">
+      </embed>
     </div>
   )
 }

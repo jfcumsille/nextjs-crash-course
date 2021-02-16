32347839ddcb560de2e0f65473a1b05f4c5b1a01
jfcumsille <jfcumsille@uc.cl>
2021-02-16 09:29:31 -0300

    trying with allow all

--- pages/iframe.js
+++ pages/iframe.js
@@ -10,7 +10,7 @@ const iframe = () => {
           title="Test Continuum"
           width="1024"
           height="720"
-          src="https://2a26269777f5.ngrok.io/applicant?continuum_token=token">
+          src="https://681aea312ca8.ngrok.io/applicant?continuum_token=token">
       </embed>
     </div>
   )
